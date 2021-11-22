5ba52528f88f06271cfe950b86bb00a70ef79661
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-22 13:56:00 +0100

    struct

---
+++ brief.c
@@ -0,0 +1,15 @@
+
+ #include<stdio.h>
+  
+int main()
+   {
+       struct CompteBancaire{
+           char CIN[50];
+           char Nom[100];
+           char Prenom[100];
+           float Montant;
+           };
+       
+      return 0;
+   }
+ 
