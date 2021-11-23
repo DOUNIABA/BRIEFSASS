19c87acf8fcd145c9292c997d43040731e777b1e
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-23 11:00:58 +0100

    menu/ajout/AFFICHER/RECHERCHER/SUPP

--- brief.c
+++ brief.c
@@ -17,21 +17,21 @@ struct Comptebancaire compte;
         C=fopen("comptes.txt","a");
         printf("entrez votre CIN :");
         scanf("%s", &compte.CIN);
-        fprintf(C," %s ",compte.CIN);
+        fprintf(C," %s \n",compte.CIN);
         fflush(stdin);
        
         printf("entrez votre nom :");
         scanf("%s",&compte.Nom);
-        fprintf(C,"%s ",compte.Nom);
+        fprintf(C," \n %s \n ",compte.Nom);
         fflush(stdin);
          printf("entrez prenom :");
         scanf("%s", &compte.Prenom);
-        fprintf(C,"%s ",compte.Prenom);
+        fprintf(C,"\n %s \n",compte.Prenom);
         fflush(stdin);
 
         printf("entrez montant :");
         scanf("%f",&compte.Montant);
-        fprintf(C,"%f ",compte.Montant);
+        fprintf(C,"\n %f \n",compte.Montant);
         fflush(stdin);
         fclose(C);
         printf("compte bien cree");
@@ -39,7 +39,7 @@ struct Comptebancaire compte;
 
 
        void AfficherCompte(){
-        FILE *C;
+        FILE * C;
         C=fopen("comptes.txt","r");
 
       
@@ -64,13 +64,14 @@ void rechercher(){
 char Numrech;
 FILE * C;
 C =fopen("comptes.txt","a");
-printf("donner un num de recherche :");
-
+printf("donner un numero de recherche :");
 
 do {
+
+    if (compte.CIN==Numrech){
 fscanf(C,"%s ,%s , %s , %f", &compte.CIN, &compte.Nom, &compte.Prenom, &compte.Montant);
 fflush(stdin);
-if (compte.CIN==Numrech){
+
 printf("CIN\t: %s" ,compte.CIN);
 printf("CIN\t: %s", compte.Nom);
 printf("CIN\t: %s", compte.Prenom);
@@ -80,20 +81,38 @@ fclose(C);
 }
 }
 
+void supprimer(){
+char CIN_RECH;
+FILE * C;
+C =fopen("comptes.txt","a");
+printf("\n\n\n\tEntrez le CIN du client a effacer: ");
+scanf("%s",CIN); 
 
+if(CIN_RECH==CIN)
+{
+printf("\tLe fichier n'exiqte pas!!!\n",CIN_RECH);
+}
+else
+{
+printf("\n\tLe compte de %s  est bien efface.\n",CIN_RECH);
+}
+break;
+
+}
+}
 int main(){
 char choix ;
 char qus;
 do{
 system("cls");
-printf("MENU \n");
+printf("\n Menu \n");
 printf("1 Ajouter un nouveau compte");
 printf("\n 2 Affichage des comptes");
 printf("\n 3.Rechercher");
 printf("\n 4. Supprimer");
 printf("\n 5.Quitter");
 do{
-printf("\n Entrez votre choix");
+printf("\n Entrez votre choix:\t");
 scanf("%d",&choix);
 }while(choix<1 || choix>5);
 switch(choix){
@@ -106,10 +125,13 @@ break;
 case 3:
 rechercher();
 break;
+case 3:
+supprimer();
+break;
 
 }
 
-printf("\n continuez O/N?");
+printf("\n continuez ");
 scanf("%s",&qus);
 fflush(stdin);
 }while (qus=='o' || qus=='O' );
